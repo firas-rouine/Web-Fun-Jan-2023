5c9c35659c3912e5c068e89b71950fa7509a7cc3
firas-rouine <rouine.firas@gmail.com>
2023-05-10 21:58:58 +0100

    Liking Likes commit

---
+++ Week2/Day 2/Core/Liking Likes/Part I/script.js
@@ -0,0 +1,6 @@
+var like =document.querySelector(".nbr").innerHTML
+var inc=document.querySelector(".nbr")
+function increase(){
+    like++
+    inc.innerHTML=like
+}
---
+++ Week2/Day 2/Core/Liking Likes/Part II/script.js
@@ -0,0 +1,14 @@
+var like1 =document.querySelector(".nbr1").innerHTML
+var inc1=document.querySelector(".nbr1")
+var like2 =document.querySelector(".nbr2").innerHTML
+var inc2=document.querySelector(".nbr2")
+var like3 =document.querySelector(".nbr3").innerHTML
+var inc3=document.querySelector(".nbr3")
+function increase(){
+    like1++
+    inc1.innerHTML=like1
+    like2++
+    inc2.innerHTML=like2
+    like3++
+    inc3.innerHTML=like3
+}
