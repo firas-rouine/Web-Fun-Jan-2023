629d7deefe0813bc3aaacb34c47072188305ea2c
firas-rouine <rouine.firas@gmail.com>
2023-05-12 15:47:39 +0100

    update

--- Week2/Day 2/Core/Liking Likes/Part II/script.js
+++ Week2/Day 2/Core/Liking Likes/Part II/script.js
@@ -4,11 +4,15 @@ var like2 =document.querySelector(".nbr2").innerHTML
 var inc2=document.querySelector(".nbr2")
 var like3 =document.querySelector(".nbr3").innerHTML
 var inc3=document.querySelector(".nbr3")
-function increase(){
+function increase1(){
     like1++
     inc1.innerHTML=like1
+}
+function increase2(){
     like2++
     inc2.innerHTML=like2
+}
+function increase3(){
     like3++
     inc3.innerHTML=like3
 }
