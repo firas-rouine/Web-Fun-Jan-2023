05e9131446cfa2a047624d1ad3a1b9192c25a003
firas-rouine <rouine.firas@gmail.com>
2023-05-26 10:50:26 +0100

    users CRUD PUSH

---
+++ Python-23/Week2/Day 3/Practice/Users CRUD/server.py
@@ -0,0 +1,48 @@
+from flask import Flask,render_template, redirect,request
+from user_model import User
+app = Flask(__name__)
+
+@app.route('/users')
+def index():
+    users = User.get_all()
+    return render_template("read_all.html",users=users)
+
+@app.route('/users/new')
+def new_user():
+    return render_template("create.html")
+
+@app.route('/users', methods=['POST'])
+def create():
+    User.create(request.form)
+    return redirect('/users')
+
+@app.route('/users/<int:user_id>')
+def one_user(user_id):
+    data = {
+        'id':user_id
+    }
+    user = User.get_one(data)
+    return render_template('read_one.html',user=user)
+@app.route('/users/<int:user_id>/edit')
+def edit_user(user_id):
+    user = User.get_one({'id':user_id})
+    return render_template("edit_user.html",user=user)
+
+@app.route('/users/<int:user_id>/edit', methods=['POST'])
+def update_user(user_id):
+    data = {
+        "first_name":request.form["first_name"],
+        "last_name":request.form["last_name"],
+        "email":request.form["email"],
+        "id":user_id
+        }
+    User.update(data)
+    return redirect('./edit')
+
+@app.route('/users/<int:user_id>/delete')
+def delete(user_id):
+    User.delete(user_id)
+    return redirect('/users')
+
+if __name__ == '__main__':
+    app.run(debug =True)
